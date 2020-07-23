1c3e6e27303e66c4bfbfe3e5f1137f361996cc3d
Grant Moss <gmoss@ualberta.ca>
2020-07-23 10:20:42 -0600

    kind of got unit switching to work

--- Colette/crude_by_rail.js
+++ Colette/crude_by_rail.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded',()=>{
     const chart = Highcharts.chart('container', {
 
         chart:{
-            type:'line', //line,bar,scatter,area,areaspline
+            type:'area', //line,bar,scatter,area,areaspline
             zoomType: 'x', //allows the user to focus in on the x or y (x,y,xy)
             borderColor: 'black',
             borderWidth: 1,
@@ -15,12 +15,13 @@ document.addEventListener('DOMContentLoaded',()=>{
             href:'https://www.cer-rec.gc.ca/index-eng.html'
         },
 
-        title:{text:'Global Crude Prices'},
+        title:{text:'Crude by Rail Exports'},
 
 
         data: {
             //csv: document.getElementById('csv').innerHTML
-            csvURL: 'https://raw.githubusercontent.com/mbradds/HighchartsData/master/oil_price.csv'
+            csvURL: 'https://raw.githubusercontent.com/mbradds/HighchartsData/master/crude_by_rail.csv',
+            seriesMapping : [{x:0,y:1}]
         },
 
         colors:['#054169','#FFBE4B','#5FBEE6','#559B37','#FF821E','#871455','#FFFFFF','#8c8c96','#42464B'],
@@ -40,17 +41,15 @@ document.addEventListener('DOMContentLoaded',()=>{
         },
 
         tooltip:{
-            //animation:false //tooltip jumps around
-            //backgroundColor: #111111 
-            //style:{
-            //    color:'#ffffff' //changes the text color
-            //}
             shared:true,
         },
 
+        xAxis: {
+            type: 'datetime'
+        },
+
         yAxis: {
-            title:{text:'Crude Oil Prices'},
-            min : 0
+            title:{text:'Crude by Rail Exports (bbl/day)'}
         }
 
     });
---
+++ Colette/crude_by_rail_units.js
@@ -0,0 +1,73 @@
+
+var select = document.getElementById('select');
+select.addEventListener('change', (select) => {
+
+    var units = select.target.value;
+   
+        Highcharts.chart('container', {
+    
+            chart:{
+                type:'area', //line,bar,scatter,area,areaspline
+                zoomType: 'x', //allows the user to focus in on the x or y (x,y,xy)
+                borderColor: 'black',
+                borderWidth: 1,
+                animation: false
+            },
+    
+            credits:{
+                //enabled:false //gets rid of the "Highcharts logo in the bottom right"
+                text:'Canada Energy Regulator',
+                href:'https://www.cer-rec.gc.ca/index-eng.html'
+            },
+    
+            title:{text:'Crude by Rail Exports'},
+    
+            data: {
+                csvURL: 'https://raw.githubusercontent.com/mbradds/HighchartsData/master/crude_by_rail_units.csv',
+                complete: function(options) {
+                    options.series = options.series.filter(data => data.name === units)
+            }
+            },
+    
+    
+            colors:['#054169','#FFBE4B','#5FBEE6','#559B37','#FF821E','#871455','#FFFFFF','#8c8c96','#42464B'],
+    
+            
+            plotOptions: {
+                series: {
+                    stickyTracking:false,
+                    marker: {
+                        enabled: false
+                    },
+                    states: {
+                        inactive: {
+                          opacity: 1
+                        }
+                      }
+                    
+                }
+            },
+    
+            tooltip:{
+                shared:true
+            },
+    
+            xAxis: {
+                type: 'datetime'
+            },
+    
+            yAxis: {
+                title:{text:'Crude by Rail Exports'+'('+units+')'}
+            }
+    
+        });
+
+        Highcharts.chart.redraw()
+    
+    });
+
+
+
+
+
+
