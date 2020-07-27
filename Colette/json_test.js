453e4a5f86b9937d15070e02fed0035704aabd69
Grant Moss <gmoss@ualberta.ca>
2020-07-27 13:54:35 -0600

    finished crude by rail dual axis

--- Colette/json_test.js
+++ Colette/json_test.js
@@ -1,5 +1,6 @@
 const xhr = new XMLHttpRequest();
-const url = 'https://raw.githubusercontent.com/mbradds/HighchartsData/master/crude_by_rail.json';
+const url = 'https://raw.githubusercontent.com/mbradds/HighchartsData/master/crude_by_rail_wcs.json';
+var githubData = JSON.parse(JSON.stringify(JSON.parse(getData(url))));
 
 function getData(Url){
     var Httpreq = new XMLHttpRequest(); // a new request
@@ -8,78 +9,160 @@ function getData(Url){
     return Httpreq.responseText;          
 };
 
-function filterData(jsdata){
-    jsdata = jsdata.filter(row => row['Units'] === "bbl per day");
+var dataMap = [{'x':'Date','y':'Volume'},{'x':'Date','y':'WCS Differential'}];
+
+function filterData(jsdata,map,filter_value='bbl per day'){
+    jsdata = jsdata.filter(row => row['Units'] === filter_value);
     jsdata = jsdata.filter(row => delete row['Units']);
-    jsdata = jsdata.filter(row => row['x'] = row['Date']);
-    jsdata = jsdata.filter(row => row['y'] = row['Volume']);
-    jsdata = jsdata.filter(row => delete row['Date']);
-    jsdata = jsdata.filter(row => delete row['Volume']);
+    for (var key of Object.keys(map)){
+        jsdata = jsdata.filter(row => row[key] = row[map[key]]);
+        jsdata = jsdata.filter(row => delete row[map[key]]);
+    }
+    
+    //TODO: remove extra json columns here
+    jsdata = jsdata.map(function(elt) {return (({ x, y }) => ({ x, y }))(elt);});
+
+    jsdata = {'name':map['y'],
+              'data':jsdata}
+
     return jsdata
 }
 
-var json_obj = filterData(JSON.parse(getData(url)));
+function mapData(filter_value){
 
+    var json_obj = [];
+    for (i=0;i<dataMap.length;i++){
+        var data = JSON.parse(JSON.stringify(githubData)); //deep copy so that only one github request is made
+        json_obj.push(filterData(data,dataMap[i],filter_value));
+    }
+
+    return json_obj
+}
+
+json_obj = mapData('bbl per day')
 console.log(json_obj)
 
-document.addEventListener('DOMContentLoaded',()=>{
-    Highcharts.chart('container', {
+var chart = new Highcharts.chart('container', {
  
-            chart:{
-                type:'area', //line,bar,scatter,area,areaspline
-                zoomType: 'x', //allows the user to focus in on the x or y (x,y,xy)
-                borderColor: 'black',
-                borderWidth: 1,
-                animation: false,
-                events:{
-                    load: function() {
-                        this.credits.element.onclick = function() {
-                            window.open(
-                              'https://www.cer-rec.gc.ca/index-eng.html',
-                              '_blank' // <- This is what makes it open in a new window.
-                            );
-                         }
-                    }
-                }   
-            },
+        chart:{
+            type:'area', //line,bar,scatter,area,areaspline
+            zoomType: 'x', //allows the user to focus in on the x or y (x,y,xy)
+            borderColor: 'black',
+            borderWidth: 1,
+            animation: false,
+            events:{
+                load: function() {
+                    this.credits.element.onclick = function() {
+                        window.open(
+                            'https://www.cer-rec.gc.ca/index-eng.html',
+                            '_blank' // <- This is what makes it open in a new window.
+                        );
+                     }
+                }
+            }   
+        },
     
-            credits:{
-                //enabled:false //gets rid of the "Highcharts logo in the bottom right"
-                text:'Canada Energy Regulator',
-                href:'https://www.cer-rec.gc.ca/index-eng.html'
-            },
+        credits:{
+            //enabled:false //gets rid of the "Highcharts logo in the bottom right"
+            text:'Canada Energy Regulator',
+            href:'https://www.cer-rec.gc.ca/index-eng.html'
+        },
 
-            legend: {
-                enabled: false
-            },
+        legend: {
+            enabled: false
+        },
     
-            title:{text:'Crude by Rail Exports'},
+        title:{text:'Crude by Rail Exports'},
     
-            colors:['#054169','#FFBE4B','#5FBEE6','#559B37','#FF821E','#871455','#FFFFFF','#8c8c96','#42464B'],
+        colors:['#054169','#FFBE4B','#5FBEE6','#559B37','#FF821E','#871455','#FFFFFF','#8c8c96','#42464B'],
     
-            tooltip:{
-                shared:true,
-            },
+        tooltip:{
+            shared:true,
+        },
 
-            series: [{
-                data: json_obj
-            }],
+        series: [{
+            type: 'area',
+            yAxis: 0,
+            name: json_obj[0]['name'],
+            data: json_obj[0]['data'],
+            color: '#054169'
+        },
+        {
+            type: 'line',
+            yAxis: 1,
+            name: json_obj[1]['name'],
+            data: json_obj[1]['data'],
+            color: '#FFBE4B'
+        }],
     
-            // xAxis: {
-            //     type: 'datetime',
-            //     dateTimeLabelFormats: {
-            //         day: '%e of %b'
-            //     }
-            // },
-    
-            yAxis: {
-                title:{text: 'bbl per day'}
+        xAxis: {
+            type: 'datetime',
+            dateTimeLabelFormats: {
+                day: '%e of %b'
             }
+        },
+        yAxis: [{ // Primary yAxis
+            labels: {
+                format: '{value}',
+                style: {
+                    color: 'black'
+                }
+            },
+            title: {
+                text: 'Rail Exports - bbl per day',
+                style: {
+                    color: 'black'
+                }
+            }
+        }, { // Secondary yAxis
+            title: {
+                text: 'Differential - USD/bbl',
+                style: {
+                    color: 'black'
+                }
+            },
+            labels: {
+                format: '{value}',
+                style: {
+                    color: 'black'
+                }
+            },
+            opposite: true
+        }],
             
-        });
-        
     });
+        
 
+var select = document.getElementById('select');
+select.addEventListener('change', (select) => {
 
+    var units = select.target.value;
+    json_obj = mapData(units)
+            
+    Highcharts.charts.forEach((graph) => {
+    
+        graph.update({
+            series: [{
+                type: 'area',
+                name: json_obj[0]['name'],
+                data: json_obj[0]['data'],
+                color: '#054169'
+           },
+           {
+               type: 'line',
+               name: json_obj[1]['name'],
+               data: json_obj[1]['data'],
+               color: '#FFBE4B'
+           }],
 
-//console.log(Highcharts.charts)
+           yAxis: {
+            title:{text: 'Rail Exports - '+units}
+        }
+            
+        })
+                
+        graph.redraw(true);
+                
+    });
+        
+});
