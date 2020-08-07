595371ee17f05276058f4a5336477bc45286c75b
Grant Moss <gmoss@ualberta.ca>
2020-08-07 16:54:47 -0600

    completed ngl without css

--- Ryan/ngl_exports.js
+++ Ryan/ngl_exports.js
@@ -14,45 +14,82 @@ var dataMap = [{'x':'Period','y':'Pipeline'},
                {'x':'Period','y':'Truck'},
             ];
 
-var filterMap = {'Product':'Propane',
-                 'Region':'British Columbia',
-                 'Units':'m3'}
+//put the filters in order or dependency
+var filterMap = {'Product':{'Value':'Propane','Dependent':false},
+                 'Units':{'Value':'bbl','Dependent':false},
+                 'Region':{'Value':'Canada','Dependent':true}
+                 }
 
 
-function getUnique(items,filterColumns){
-    var lookup2 = [];
-    var result2 = {};
+function dynamicDropDown(id,optionsArray){
 
-    for(f in filterColumns){
-        lookup2.push({})
-        result2[filterColumns[f]] = []
+    function addOption(id,text,select){
+        select.options[select.options.length] = new Option(text);
     }
+                
+    var select = document.getElementById(id);
+    select.options.length = 0;
+                
+    for (var i = 0; i < optionsArray.length; i++) {
+            addOption (id, optionsArray[i],select);
+        }
+                
+}
 
-    for (var item, i = 0; item = items[i++];) {
+//gets the unique regions to populate the dropdown
+function getUnique(items,filterColumns){
+    if (Array.isArray(filterColumns)){
+        var lookup = [];
+        var result = {};
+    
         for(f in filterColumns){
-            var name = item[filterColumns[f]];
-            if(!(name in lookup2[f])) {
-                lookup2[f][name] = 1;
-                result2[filterColumns[f]].push(name);
+            lookup.push({})
+            result[filterColumns[f]] = []
+        }
+    
+        for (var item, i = 0; item = items[i++];) {
+            for(f in filterColumns){
+                var name = item[filterColumns[f]];
+                if(!(name in lookup[f])) {
+                    lookup[f][name] = 1;
+                    result[filterColumns[f]].push(name);
+                }
+                
             }
-            
+    
+        }
+        return result
+        } else {
+            var lookup = {};
+            var result = [];
+            for (var item, i = 0; item = items[i++];) {
+                var name = item[filterColumns];
+                if(!(name in lookup)) {
+                    lookup[name] = 1;
+                    result.push(name);
+                    }
+            }
+        return result
+            }
         }
 
-    }
-    return result2
-    }
 
 var drop = getUnique(githubData,filterColumns=Object.keys(filterMap))
-console.log(drop);
-
+dynamicDropDown("select_region", drop['Region'].sort())
+document.getElementById('select_region').value = 'Canada';
 
 function filterData(data,map,filterMap){
     //this for filters the data based on the default user parameters
     for (var key of Object.keys(filterMap)){
-        data = data.filter(row => row[key] == filterMap[key]);
+        //this section of code gets all the valid provinces given the product
+        //TODO: update the dropdown here with relevant regions
+        // if(filterMap[key]['Dependent'] == true){
+        //     var drop = getUnique(data,filterColumns=key)
+        // }
+        data = data.filter(row => row[key] == filterMap[key]['Value']);
         data = data.filter(row => delete row[key]);
     }
-
+    
     var xyData = [];
     
     for (var row of data){
@@ -64,7 +101,7 @@ function filterData(data,map,filterMap){
 
     data = {'name':map['y'],
               'data':xyData}
-
+    //dynamicDropDown("select_region", drop)
     return data
 }
 
@@ -74,7 +111,7 @@ function mapData(filterMap){
     for (i=0;i<dataMap.length;i++){
         var data = JSON.parse(JSON.stringify(githubData)); //deep copy so that only one github request is made
         var hcReady = filterData(data,dataMap[i],filterMap)
-        //data has nulls removed at this point
+        //TODO: remove series objects "columns" that are empty
         //if (hcReady['data'].length > 0){
         //    json_obj.push(hcReady);
         //}
@@ -94,12 +131,33 @@ const chart = new Highcharts.chart('container', {
             type:'line', //line,bar,scatter,area,areaspline
             zoomType: 'x', //allows the user to focus in on the x or y (x,y,xy)
             borderColor: 'black',
-            borderWidth: 1
+            borderWidth: 1,
+            events:{
+                load: function() {
+                    this.credits.element.onclick = function() {
+                        window.open(
+                          'https://www.cer-rec.gc.ca/index-eng.html',
+                          '_blank' // <- This is what makes it open in a new window.
+                        );
+                     }
+                }
+            }   
+        },
+
+        credits:{
+            //enabled:false //gets rid of the "Highcharts logo in the bottom right"
+            text:'Canada Energy Regulator',
+            href:'https://www.cer-rec.gc.ca/index-eng.html'
         },
 
         plotOptions: {
             series: {
-                connectNulls: false 
+                connectNulls: false,
+                states: {
+                    hover: {
+                    enabled: false
+                    }
+                    } 
             }
         },
 
@@ -107,7 +165,7 @@ const chart = new Highcharts.chart('container', {
             shared:true,
         },
     
-        title:{text: 'Propane Exports'},
+        title:{text: 'Canada Propane Exports'},
     
         colors:['#054169','#FFBE4B','#5FBEE6','#559B37','#FF821E','#871455','#FFFFFF','#8c8c96','#42464B'],
 
@@ -119,7 +177,18 @@ const chart = new Highcharts.chart('container', {
         },
 
         yAxis: {
-            title:{text: 'Propane Exports'}
+            title:{text: 'bbl'}
+        },
+
+        lang: {
+            noData: "No Exports"
+        },
+        noData: {
+            style: {
+                fontWeight: 'bold',
+                fontSize: '15px',
+                color: '#303030'
+            }
         },
 
         series: [{
@@ -150,10 +219,11 @@ const chart = new Highcharts.chart('container', {
     });
 
 
-function graphEvent(product,units){
+function graphEvent(product,units,region){
 
-    filterMap['Product'] = product
-    filterMap['Units'] = units
+    filterMap['Product']['Value'] = product
+    filterMap['Units']['Value'] = units
+    filterMap['Region']['Value'] = region
     
     json_obj = mapData(filterMap)
     Highcharts.charts.forEach((graph) => {
@@ -184,10 +254,10 @@ function graphEvent(product,units){
                 color: '#559B37'
             }],
 
-            title:{text: product+' Exports'},
+            title:{text: region+' '+product+' Exports'},
 
             yAxis: {
-                title:{text: product+' Exports'}
+                title:{text: units}
             },
             })
                     
@@ -200,17 +270,23 @@ function graphEvent(product,units){
 var select_product = document.getElementById('select_product');
 select_product.addEventListener('change', (select_product) => {
     var product = select_product.target.value;
-    var units = filterMap['Units']
-    graphEvent(product=product,units=units)
-
-            
+    var units = filterMap['Units']['Value']
+    var region = filterMap['Region']['Value']
+    graphEvent(product=product,units=units,region=region)
 });       
 
 var select_units = document.getElementById('select_units');
 select_units.addEventListener('change', (select_units) => {
     var units = select_units.target.value;
-    var product = filterMap['Product']
-    graphEvent(product=product,units=units)
-
-            
+    var product = filterMap['Product']['Value']
+    var region = filterMap['Region']['Value']
+    graphEvent(product=product,units=units,region=region)
 });       
+
+var select_region = document.getElementById('select_region');
+select_region.addEventListener('change', (select_region) => {
+    var region = select_region.target.value;
+    var product = filterMap['Product']['Value']
+    var units = filterMap['Units']['Value']
+    graphEvent(product=product,units=units,region=region)      
+});   
