7559a2a49e44c024c6e3e703ea2e9204a0c35a6f
Grant Moss <gmoss@ualberta.ca>
2020-08-28 17:09:04 -0600

    completed first draft of oil

--- Jennifer/oil/oil_throughputs.js
+++ Jennifer/oil/oil_throughputs.js
@@ -1,11 +1,65 @@
-const createChart = (githubData,filterMap) => {
+const blankChart = () => {
+    Highcharts.setOptions({lang: {noData: "Click on a Pipeline Key Point"}})
+    var chart = Highcharts.chart('container_chart', {
+          chart: {
+          renderTo: 'container_chart'
+        },
+            title: {
+            text: ''
+        },
+        
+        credits: {
+            enabled: false
+        },
+        
+        yAxis: {
+          title: {
+             text: ''
+          },
+        }, 
+    
+        xAxis: {
+           type: 'datetime'
+        },
+        
+        legend: {
+           enabled: false
+        },
+        
+        series: [{
+            data: []
+        }]
+    });
 
-    var throughcap = filterDataSeries(githubData,filterMap) 
+    return chart
+}
+
+//takes in a json object and checks if the column has data
+const checkIfValid = (data) =>{
+    let valid = false
+    for (t = 0; t < data.length; t++) {
+        if (data[t]['y'] != null && data[t]['y'] != 0) {
+            valid = true;
+            break;
+        }
+    };
+    return valid
+}   
+    
+const createChart = (githubData,filterMap,colorsCapacity,colorsThroughput,y) => {
+    
+    var throughcap = filterDataSeries(githubData,filterMap,colorsCapacity,colorsThroughput,y) 
     var throughput = throughcap.slice(0)[0]
     var capacity = throughcap.slice(-1)[0]
-    var data = throughput.concat(capacity); 
+    var showCap = checkIfValid(capacity.data)
+    if (showCap){
+        var data = throughput.concat(capacity);    
+    } else {
+        var data = throughput
+    }
+        
     const chart = new Highcharts.chart('container_chart', {
-
+    
         chart: {
             renderTo: 'container_chart',
             //type: 'area', //line,bar,scatter,area,areaspline
@@ -24,13 +78,13 @@ const createChart = (githubData,filterMap) => {
                 }
             }
         },
-
+    
         credits: {
             //enabled:false //gets rid of the "Highcharts logo in the bottom right"
             text: 'Canada Energy Regulator',
             href: 'https://www.cer-rec.gc.ca/index-eng.html'
         },
-
+    
         plotOptions: {
             area: {
                 stacking: 'normal'
@@ -48,29 +102,30 @@ const createChart = (githubData,filterMap) => {
                 }
             }
         },
-
+    
         tooltip: {
             animation: true,
             shared: true
         },
-
+    
         title: {text: filterMap['Corporate Entity']['Value']+' '+filterMap['Key Point']['Value']},
-
+    
         colors: ['#054169', '#FFBE4B', '#5FBEE6', '#559B37', '#FF821E', '#871455', '#8c8c96', '#42464B'],
-
+    
         yAxis: {
             title: {
                 text: 'Throughput and Capacity'
             },
         }, 
-
+    
         xAxis: {
             type: 'datetime'
         },
-
+    
         lang: {
             noData: "Click on a key point"
         },
+
         noData: {
             style: {
                 fontWeight: 'bold',
@@ -78,32 +133,33 @@ const createChart = (githubData,filterMap) => {
                 color: '#303030'
             }
         },
+
         series: data
-    });
 
-    //return chart
-}
-
-//const chart = createChart(githubData,filterMap);
-
+        });
+    
+        return chart
+    }
+    
+    
 const getData = (Url) => {
     var Httpreq = new XMLHttpRequest(); // a new request
     Httpreq.open("GET", Url, false);
     Httpreq.send(null);
     return Httpreq.responseText;
 };
-
+    
 //gets the unique regions to populate the dropdown
 const getUnique = (items, filterColumns) => {
     if (Array.isArray(filterColumns)) {
         var lookup = [];
         var result = {};
-
-        for (f in filterColumns) {
+    
+        for (var f in filterColumns) {
             lookup.push({})
             result[filterColumns[f]] = []
         }
-
+    
         for (var item, i = 0; item = items[i++];) {
             for (f in filterColumns) {
                 var name = item[filterColumns[f]];
@@ -113,8 +169,9 @@ const getUnique = (items, filterColumns) => {
                 }
             }
         }
-        return result
 
+        return result
+    
     } else {
         var lookup = {};
         var result = [];
@@ -128,19 +185,22 @@ const getUnique = (items, filterColumns) => {
         return result
     }
 }
-
-const groupBy = (itter,column) => {
+    
+const groupBy = (itter,column,colorsCapacity) => {
+    //get the appropriate color
+    var capColor = colorsCapacity[itter[0]['Corporate Entity']]
+    
     result = {}
     grouped = itter.reduce((result,current) => {
         result[current[column]] = result[current[column]] || [];
         result[current[column]].push(current['Available Capacity (1000 m3/d)']);
         return result
     })
-
+    
     const arrAvg = arr => arr.reduce((a,b) => a + b, 0) / arr.length
-
+    
     hcGroup = []
-
+    
     for (const [key, value] of Object.entries(grouped)) {
         if (Array.isArray(value)){
             hcGroup.push(
@@ -150,103 +210,117 @@ const groupBy = (itter,column) => {
         } else {
             delete grouped[key]
         }
-      }
-    
-
+    }
+        
     var completedMetric = {
         name: 'Available Capacity (1000 m3/d)',
         data: hcGroup,
-        type: 'line'
+        type: 'line',
+        color: capColor
     }
-    
+        
     return completedMetric
 }
-
-const filterDataSeries = (data, fm) => { 
+    
+const filterDataSeries = (data, fm,colorsCapacity,colorsThroughput,y) => { 
     //get the specific pipeline
     Object.keys(fm).map((v,i) => {
         data = data.filter(row => row[v] == fm[v]['Value'])
     })
-
+    
     var capacity =  JSON.parse(JSON.stringify(data)); //deep copy so changes dont get applied to throughput
-    capacity = groupBy(capacity,'Date')
-
+    capacity = groupBy(capacity,'Date',colorsCapacity)
     var products = getUnique(data,'Product')
     var throughput = [];
-    for (product in products){
+    for (var product in products){
+        var color = colorsThroughput[products[product]]
         dataProduct = data.filter(row => row.Product == products[product])
         dataProduct = dataProduct.map((v,i) =>{
             hcRow = {
                 x: v['Date'],
-                y: v['Throughput (1000 m3/d)']
+                y: v[y]
             }
             return hcRow
         });
-
+    
         var completedMetric = {
             'name': products[product],
             'data': dataProduct,
-            'type': 'area'
+            'type': 'area',
+            'color': color
         };
-
+    
         throughput.push(completedMetric)
     }
-
     return [throughput,capacity]
 }
-
-
+        
 const filterDataPoints = (data, fm, colors) => { 
-    
+        
     data = data.filter(row => row.Commodity == fm.Commodity.Value)
     const nameChange = [['Longitude','lon'],['Latitude','lat'],['Key Point','name']]
     data = data.map((v,i) => {
-    	for (var name in nameChange){
-      	newName = nameChange[name]
-        v[newName.slice(-1)[0]] = v[newName.slice(0)[0]]
-        delete v[newName.slice(0)[0]]
-      }
-			v['color'] = colors[v['Corporate Entity']]
-      return v
+        for (var name in nameChange){
+            newName = nameChange[name]
+            v[newName.slice(-1)[0]] = v[newName.slice(0)[0]]
+            delete v[newName.slice(0)[0]]
+        }
+        v['color'] = colors[v['Corporate Entity']]
+        return v
     })
-
     return data
 }
-
+    
 const urlPoints = 'https://raw.githubusercontent.com/mbradds/HighchartsData/master/keyPoints.json'
 var githubPoints = JSON.parse(JSON.stringify(JSON.parse(getData(urlPoints))));
 const urlSeries = 'https://raw.githubusercontent.com/mbradds/HighchartsData/master/oil_throughcap.json'
 var githubSeries = JSON.parse(JSON.stringify(JSON.parse(getData(urlSeries))));
 var filterPoint = {
-    'Commodity': {'Value': 'Oil', 'Dependent': false}
+    'Commodity': {'Value': 'Oil'}
 }
-const colors = {
+const colorsCapacity = {
     'Trans Mountain Pipeline ULC': '#FFBE4B',
-    'Enbridge Pipelines (NW) Inc': '#054169',
-    'Enbridge Pipelines Inc':'#5FBEE6',
-    'Kinder Morgan':'#559B37'
-    }
+    'Enbridge Pipelines (NW) Inc.': '#054169',
+    'Enbridge Pipelines Inc.':'#5FBEE6',
+    'PKM Cochin ULC':'#559B37',
+    'TransCanada Keystone Pipeline GP Ltd.':'#FF821E'
+}
+
+const colorsThroughput = {
+    'refined petroleum products': '#5FBEE6',
+    'foreign light': '#FF821E',
+    'domestic heavy':'#871455',
+    'domestic light / ngl':'#8c8c96',
+    'domestic light':'#8c8c96'
+}
 
 var filterMap = {
     'Corporate Entity': {'Value': 'Enbridge Pipelines Inc.'},
     'Key Point':{'Value':'ex-Gretna'}
 }
+    
+var oilPoints = filterDataPoints(githubPoints,filterPoint,colorsCapacity)
+var blank = blankChart()
 
-var oilPoints = filterDataPoints(githubPoints,filterPoint,colors)
+const crudeMap = (colorsCapacity,colorsThroughput,y) =>{
 
-//Create the chart
-Highcharts.mapChart('container_map', {
+    Highcharts.mapChart('container_map', {
 
-		plotOptions: {
-      series: {
-          stickyTracking: false,
-          point: {
+        credits: {
+            //enabled:false //gets rid of the "Highcharts logo in the bottom right"
+            text: '',
+        },
+    
+        plotOptions: {
+          series: {
+              stickyTracking: false,
+              point: {
                     events: {
                         click: function () {
-                            var text = '<b>'+this['Corporate Entity']+' '+this.name+'</b>'+'<br>Series: ' + this.series.name
+                            var text = '<b>'+this['Corporate Entity']+' '+this.name+'</b>'+'<br>Direction of flow: ' + this['Direction of Flow']
                                 chart = this.series.chart;
                             if (!chart.clickLabel) {
-                                chart.clickLabel = chart.renderer.label(text, 0, 250)
+                                chart.clickLabel = chart.renderer.label(text, 525, 200)
                                     .css({
                                         width: '180px'
                                     })
@@ -256,54 +330,93 @@ Highcharts.mapChart('container_map', {
                                     text: text
                                 });
                             }
-                         filterMap['Corporate Entity'].Value = this['Corporate Entity']
-                         filterMap['Key Point'].Value = this.name
-                         createChart(githubSeries,filterMap)   
+                            filterMap['Corporate Entity'].Value = this['Corporate Entity']
+                            filterMap['Key Point'].Value = this.name
+                            createChart(githubSeries,filterMap,colorsCapacity,colorsThroughput,y)   
                         }
                     }
-                }
-      }
-  	},
-    chart: {
-        map: 'countries/ca/ca-all',
-        renderTo: 'container_map'
-    },
+                }},
+    /* 					mappoint: {
+                                        cluster: {
+                                            enabled: true,
+                                            allowOverlap: false,
+                                            animation: {
+                                                duration: 450
+                                            },
+                                            layoutAlgorithm: {
+                                                type: 'grid',
+                                                gridSize: 7
+                                            },
+                                            zones: [{
+                                                from: 1,
+                                                to: 4,
+                                                marker: {
+                                                    radius: 13
+                                                }}
+                                                ]
+                                       }
+                              } */
+          
+        },
 
-    title: {
-        text: 'Crude Oil Throughput and Capacity'
-    },
-
-/*     subtitle: {
-        text: 'Source map: <a href="http://code.highcharts.com/mapdata/countries/ca/ca-all.js">Canada</a>'
-    }, */
-
-    mapNavigation: {
-        enabled: true,
-        buttonOptions: {
-            verticalAlign: 'bottom'
-        }
-    },
+        mapNavigation: {
+            enabled: true
+            },
+            
+        chart: {
+            map: 'countries/ca/ca-all',
+            renderTo: 'container_map'
+        },
     
-    tooltip: {
-    		snap: 0,
-        //headerFormat: '<b>{point.name}</b>',
-        //pointFormat: '<b>{point.name}</b><br>Lat: {point.lat}, Lon: {point.lon}'
-        formatter: function() {
-        	return '<b>'+this.point['Corporate Entity']+'-'+this.point.name +' key point'+'</b><br>'
-          +'Click on key point to view throughput & capacity'
-    }
-    },
-
-    
-    series: [{
-    	name: 'Basemap',
-      borderColor: '#606060',
-      nullColor: 'rgba(200, 200, 200, 0.2)',
-      showInLegend: false
-    },{
-        type: 'mappoint',
-        name: 'Key Points',
-        data : oilPoints
-    }]
+        title: {
+            text: 'Crude Oil Throughput and Capacity'
+        },
         
-});
+        legend: {
+            enabled: false
+        },
+    
+        // subtitle: {
+        //     text: 'Source map: <a href="http://code.highcharts.com/mapdata/countries/ca/ca-all.js">Canada</a>'
+        // },
+    
+        mapNavigation: {
+            enabled: true,
+            buttonOptions: {
+                verticalAlign: 'bottom'
+            }
+        },
+        
+        tooltip: {
+            snap: 0,
+            //headerFormat: '<b>{point.name}</b>',
+            //pointFormat: '<b>{point.name}</b><br>Lat: {point.lat}, Lon: {point.lon}'
+            formatter: function() {
+              return '<b>'+this.point['Corporate Entity']+'-'+this.point.name +' key point'+'</b><br>'
+              +'Click on key point to view throughput & capacity'
+            }
+        },
+        
+        series: [{
+            name: 'Basemap',
+            borderColor: '#606060',
+            nullColor: 'rgba(200, 200, 200, 0.2)',
+            showInLegend: false
+        },{
+        type: 'mappoint',
+            name: 'Key Points',
+            data : oilPoints,
+            dataLabels: {
+                    enabled: true,
+                    borderRadius: 7,
+                    padding: 4,
+                    format: '{point.name}',
+                    allowOverlap: false
+                },
+        }]
+            
+    }); 
+    
+    }
+
+crudeMap(colorsCapacity,colorsThroughput,y='Throughput (1000 m3/d)')
