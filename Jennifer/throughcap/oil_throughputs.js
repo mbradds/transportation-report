ba314c24b08cc65c8fe43b73bb24d4f5a649c0a2
Grant Moss <gmoss@ualberta.ca>
2020-09-17 10:06:43 -0600

    added class for abscracting graph parameters

--- Jennifer/throughcap/oil_throughputs.js
+++ Jennifer/throughcap/oil_throughputs.js
@@ -1,39 +1,3 @@
-const blankChart = () => {
-    Highcharts.setOptions({lang: {noData: "Click on a Pipeline Key Point"}})
-    var chart = Highcharts.chart('container_chart', {
-          chart: {
-          renderTo: 'container_chart'
-        },
-            title: {
-            text: ''
-        },
-        
-        credits: {
-            enabled: false
-        },
-        
-        yAxis: {
-          title: {
-             text: ''
-          },
-        }, 
-    
-        xAxis: {
-           type: 'datetime'
-        },
-        
-        legend: {
-           enabled: false
-        },
-        
-        series: [{
-            data: []
-        }]
-    });
-
-    return chart
-}
-
 //takes in a json object and checks if the column has data
 const checkIfValid = (data) =>{
     let valid = false
@@ -45,103 +9,7 @@ const checkIfValid = (data) =>{
     };
     return valid
 }   
-    
-const createChart = (githubData,filterMap,colorsCapacity,colorsThroughput,yT,yC) => {
-    
-    var throughcap = filterDataSeries(githubData,filterMap,colorsCapacity,colorsThroughput,yT,yC) 
-    var throughput = throughcap.slice(0)[0]
-    var capacity = throughcap.slice(-1)[0]
-    var showCap = checkIfValid(capacity.data)
-    if (showCap){
-        var data = throughput.concat(capacity);    
-    } else {
-        var data = throughput
-    }
-        
-    const chart = new Highcharts.chart('container_chart', {
-    
-        chart: {
-            renderTo: 'container_chart',
-            //type: 'area', //line,bar,scatter,area,areaspline
-            zoomType: 'x', //allows the user to focus in on the x or y (x,y,xy)
-            //borderColor: 'black',
-            //borderWidth: 1,
-            animation: true,
-            events: {
-                load: function () {
-                    this.credits.element.onclick = function () {
-                        window.open(
-                            'https://www.cer-rec.gc.ca/index-eng.html',
-                            '_blank' // <- This is what makes it open in a new window.
-                        );
-                    }
-                }
-            }
-        },
-    
-        credits: {
-            //enabled:false //gets rid of the "Highcharts logo in the bottom right"
-            text: 'Canada Energy Regulator',
-            href: 'https://www.cer-rec.gc.ca/index-eng.html'
-        },
-    
-        plotOptions: {
-            area: {
-                stacking: 'normal'
-            },
-            series: {
-                turboThreshold: 10000,
-                stickyTracking: false,
-                connectNulls: false,
-                states: {
-                    inactive: {
-                        opacity: 1
-                    },
-                    hover: {
-                        enabled: false
-                    }
-                }
-            }
-        },
-    
-        tooltip: {
-            animation: true,
-            shared: true
-        },
-    
-        title: {text: filterMap['Corporate Entity']['Value']+' '+filterMap['Key Point']['Value']},
-    
-        colors: ['#054169', '#FFBE4B', '#5FBEE6', '#559B37', '#FF821E', '#871455', '#8c8c96', '#42464B'],
-    
-        yAxis: {
-            title: {
-                text: yT
-            },
-        }, 
-    
-        xAxis: {
-            type: 'datetime'
-        },
-    
-        lang: {
-            noData: "Click on a key point"
-        },
 
-        noData: {
-            style: {
-                fontWeight: 'bold',
-                fontSize: '15px',
-                color: '#303030'
-            }
-        },
-
-        series: data
-
-        });
-    
-        return chart
-    }
-    
     
 const getData = (Url) => {
     var Httpreq = new XMLHttpRequest(); // a new request
@@ -284,7 +152,43 @@ const filterDataPoints = (data, colors) => {
     })
     return data
 }
+
+const blankChart = () => {
+    Highcharts.setOptions({lang: {noData: "Click on a Pipeline Key Point"}})
+    var chart = Highcharts.chart('container_chart', {
+          chart: {
+          renderTo: 'container_chart'
+        },
+            title: {
+            text: ''
+        },
+        
+        credits: {
+            enabled: false
+        },
+        
+        yAxis: {
+          title: {
+             text: ''
+          },
+        }, 
     
+        xAxis: {
+           type: 'datetime'
+        },
+        
+        legend: {
+           enabled: false
+        },
+        
+        series: [{
+            data: []
+        }]
+    });
+
+    return chart
+}
+
 const throughCapMap = (pointsData,seriesData,colorsCapacity,colorsThroughput,yT,yC) =>{
 
     var filterMap = {
@@ -407,6 +311,102 @@ const throughCapMap = (pointsData,seriesData,colorsCapacity,colorsThroughput,yT,
     
 }
 
+const createChart = (githubData,filterMap,colorsCapacity,colorsThroughput,yT,yC) => {
+    
+    var throughcap = filterDataSeries(githubData,filterMap,colorsCapacity,colorsThroughput,yT,yC) 
+    var throughput = throughcap.slice(0)[0]
+    var capacity = throughcap.slice(-1)[0]
+    var showCap = checkIfValid(capacity.data)
+    if (showCap){
+        var data = throughput.concat(capacity);    
+    } else {
+        var data = throughput
+    }
+        
+    const chart = new Highcharts.chart('container_chart', {
+    
+        chart: {
+            renderTo: 'container_chart',
+            //type: 'area', //line,bar,scatter,area,areaspline
+            zoomType: 'x', //allows the user to focus in on the x or y (x,y,xy)
+            //borderColor: 'black',
+            //borderWidth: 1,
+            animation: true,
+            events: {
+                load: function () {
+                    this.credits.element.onclick = function () {
+                        window.open(
+                            'https://www.cer-rec.gc.ca/index-eng.html',
+                            '_blank' // <- This is what makes it open in a new window.
+                        );
+                    }
+                }
+            }
+        },
+    
+        credits: {
+            //enabled:false //gets rid of the "Highcharts logo in the bottom right"
+            text: 'Canada Energy Regulator',
+            href: 'https://www.cer-rec.gc.ca/index-eng.html'
+        },
+    
+        plotOptions: {
+            area: {
+                stacking: 'normal'
+            },
+            series: {
+                turboThreshold: 10000,
+                stickyTracking: false,
+                connectNulls: false,
+                states: {
+                    inactive: {
+                        opacity: 1
+                    },
+                    hover: {
+                        enabled: false
+                    }
+                }
+            }
+        },
+    
+        tooltip: {
+            animation: true,
+            shared: true
+        },
+    
+        title: {text: `${filterMap['Corporate Entity']['Value']} ${filterMap['Key Point']['Value']}`},
+
+        colors: ['#054169', '#FFBE4B', '#5FBEE6', '#559B37', '#FF821E', '#871455', '#8c8c96', '#42464B'],
+    
+        yAxis: {
+            title: {
+                text: yT
+            },
+        }, 
+    
+        xAxis: {
+            type: 'datetime'
+        },
+    
+        lang: {
+            noData: "Click on a key point"
+        },
+
+        noData: {
+            style: {
+                fontWeight: 'bold',
+                fontSize: '15px',
+                color: '#303030'
+            }
+        },
+
+        series: data
+
+        });
+    
+        return chart
+    }
+
 const fillDrop = (column,dropName,value,data) => {
     const drop = getUnique(data, filterColumns = column)
     dynamicDropDown(dropName, drop)
@@ -414,67 +414,146 @@ const fillDrop = (column,dropName,value,data) => {
 }
 
 //main program
+
+class TrafficDashboard {
+
+    constructor(commodity){
+        this.commodity = commodity;
+        this.params = {}
+    }
+
+    properties(){
+        if (this.commodity == 'oil'){
+            this.params.urlPoints = 'https://raw.githubusercontent.com/mbradds/HighchartsData/master/keyPointsOil.json'
+            this.params.urlSeries = 'https://raw.githubusercontent.com/mbradds/HighchartsData/master/oil_throughcap.json'
+            this.params.titleText = 'Crude oil Throughput and Capacity'
+            this.params.colorsCapacity = {
+                'Trans Mountain Pipeline ULC': '#FFBE4B',
+                'Enbridge Pipelines (NW) Inc.': '#054169',
+                'Enbridge Pipelines Inc.':'#5FBEE6',
+                'PKM Cochin ULC':'#559B37',
+                'TransCanada Keystone Pipeline GP Ltd.':'#FF821E'
+            }
+            this.params.colorsThroughput = {
+                'refined petroleum products': '#5FBEE6',
+                'foreign light': '#FF821E',
+                'domestic heavy':'#871455',
+                'domestic light / ngl':'#8c8c96',
+                'domestic light':'#8c8c96'
+            }
+            this.params.yT = 'Throughput (1000 m3/d)'
+            this.params.yC = 'Available Capacity (1000 m3/d)'
+        } else if (this.commodity == 'gas'){
+            this.params.urlPoints = 'https://raw.githubusercontent.com/mbradds/HighchartsData/master/keyPointsGas.json'
+            this.params.urlSeries = 'https://raw.githubusercontent.com/mbradds/HighchartsData/master/gas_throughcap.json'
+            this.params.titleText = 'Natural gas Throughput and Capacity'
+            this.params.colorsCapacity = {
+                'Westcoast Energy Inc.':'#5FBEE6',
+                'TransCanada PipeLines Limited':'#559B37',
+                'NOVA Gas Transmission Ltd. (NGTL)':'#054169',
+                'Alliance Pipeline Limited Partnership':'#FFBE4B',
+                'Foothills Pipe Lines Ltd. (Foothills)':'#FF821E',
+                'Emera Brunswick Pipeline Company Ltd.':'#8c8c96',
+                'Maritimes & Northeast Pipeline':'#42464B',
+                'Trans Québec & Maritimes Pipeline Inc':'#871455'
+            }
+            this.params.colorsThroughput = {
+                'Natural Gas':'#42464B'
+            }
+            this.params.yT = 'Throughput (1000 m3/d)'
+            this.params.yC = 'Available Capacity (1000 m3/d)'
+
+        } else {
+            console.log('Enter a valid commodity')
+        }
+
+        return this.params
+    }
+
+    setTitle(titleText,id){
+        document.getElementById(id).innerText = titleText
+    }
+
+    get graphStructure() {
+        return this.properties()
+    }
+
+}
+
+dash = new TrafficDashboard('oil')
+console.log(dash.graphStructure)
+
 const commodityGraph = (commodity) => {
     
-    if (commodity == 'oil'){
-        var urlPoints = 'https://raw.githubusercontent.com/mbradds/HighchartsData/master/keyPointsOil.json'
-        var urlSeries = 'https://raw.githubusercontent.com/mbradds/HighchartsData/master/oil_throughcap.json'
-        var colorsCapacity = {
-            'Trans Mountain Pipeline ULC': '#FFBE4B',
-            'Enbridge Pipelines (NW) Inc.': '#054169',
-            'Enbridge Pipelines Inc.':'#5FBEE6',
-            'PKM Cochin ULC':'#559B37',
-            'TransCanada Keystone Pipeline GP Ltd.':'#FF821E'
-        }
+    // //TODO: make all of these chart variable properties into an object, or a class that takes in the commodity and creates a custom object
+    // if (commodity == 'oil'){
+
+    //     var urlPoints = 'https://raw.githubusercontent.com/mbradds/HighchartsData/master/keyPointsOil.json'
+    //     var urlSeries = 'https://raw.githubusercontent.com/mbradds/HighchartsData/master/oil_throughcap.json'
+    //     var titleText = 'Crude oil Throughput and Capacity'
+
+    //     var colorsCapacity = {
+    //         'Trans Mountain Pipeline ULC': '#FFBE4B',
+    //         'Enbridge Pipelines (NW) Inc.': '#054169',
+    //         'Enbridge Pipelines Inc.':'#5FBEE6',
+    //         'PKM Cochin ULC':'#559B37',
+    //         'TransCanada Keystone Pipeline GP Ltd.':'#FF821E'
+    //     }
         
-        var colorsThroughput = {
-            'refined petroleum products': '#5FBEE6',
-            'foreign light': '#FF821E',
-            'domestic heavy':'#871455',
-            'domestic light / ngl':'#8c8c96',
-            'domestic light':'#8c8c96'
-        }
+    //     var colorsThroughput = {
+    //         'refined petroleum products': '#5FBEE6',
+    //         'foreign light': '#FF821E',
+    //         'domestic heavy':'#871455',
+    //         'domestic light / ngl':'#8c8c96',
+    //         'domestic light':'#8c8c96'
+    //     }
 
-        var yT='Throughput (1000 m3/d)'
-        var yC='Available Capacity (1000 m3/d)'
+    //     var yT='Throughput (1000 m3/d)'
+    //     var yC='Capacity (1000 m3/d)'
 
-    } else {
+    // } else {
+
+    //     var urlPoints = 'https://raw.githubusercontent.com/mbradds/HighchartsData/master/keyPointsGas.json'
+    //     var urlSeries = 'https://raw.githubusercontent.com/mbradds/HighchartsData/master/gas_throughcap.json'
+    //     var titleText = 'Natural gas Throughput and Capacity'
         
-        var colorsCapacity = {
-            'Westcoast Energy Inc.':'#5FBEE6',
-            'TransCanada PipeLines Limited':'#559B37',
-            'NOVA Gas Transmission Ltd. (NGTL)':'#054169',
-            'Alliance Pipeline Limited Partnership':'#FFBE4B',
-            'Foothills Pipe Lines Ltd. (Foothills)':'#FF821E',
-            'Emera Brunswick Pipeline Company Ltd.':'#8c8c96',
-            'Maritimes & Northeast Pipeline':'#42464B',
-            'Trans Québec & Maritimes Pipeline Inc':'#871455'
-        }
-        var colorsThroughput = {
-            'Natural Gas':'#42464B'
-        }
-        var yT='Throughput (1000 m3/d)'
-        var yC='Capacity (1000 m3/d)'
+    //     var colorsCapacity = {
+    //         'Westcoast Energy Inc.':'#5FBEE6',
+    //         'TransCanada PipeLines Limited':'#559B37',
+    //         'NOVA Gas Transmission Ltd. (NGTL)':'#054169',
+    //         'Alliance Pipeline Limited Partnership':'#FFBE4B',
+    //         'Foothills Pipe Lines Ltd. (Foothills)':'#FF821E',
+    //         'Emera Brunswick Pipeline Company Ltd.':'#8c8c96',
+    //         'Maritimes & Northeast Pipeline':'#42464B',
+    //         'Trans Québec & Maritimes Pipeline Inc':'#871455'
+    //     }
+    //     var colorsThroughput = {
+    //         'Natural Gas':'#42464B'
+    //     }
+    //     var yT='Throughput (1000 m3/d)'
+    //     var yC='Capacity (1000 m3/d)'
 
-    }
+    // }
+
+    const dash = new TrafficDashboard(commodity)
+    const graphParams = dash.graphStructure
+
+    var title = document.getElementById("traffic_title").innerText = graphParams.titleText
+    var githubPoints = JSON.parse(JSON.stringify(JSON.parse(getData(graphParams.urlPoints))));
+    var githubSeries = JSON.parse(JSON.stringify(JSON.parse(getData(graphParams.urlSeries))));
     
     fillDrop(column='Pipeline Name',dropName='select_pipelines',value='All',data=githubSeries)
 
-
-    var pointData = filterDataPoints(githubPoints,colorsCapacity)
+    var pointData = filterDataPoints(githubPoints,graphParams.colorsCapacity)
     var blank = blankChart()
-    var pointMap = throughCapMap(pointData,githubSeries,colorsCapacity,colorsThroughput,yT,yC)
+    var pointMap = throughCapMap(pointData,githubSeries,graphParams.colorsCapacity,graphParams.colorsThroughput,graphParams.yT,graphParams.yC)
     return [pointMap,pointData]
 
 }
 
-var urlPoints = 'https://raw.githubusercontent.com/mbradds/HighchartsData/master/keyPointsGas.json'
-var urlSeries = 'https://raw.githubusercontent.com/mbradds/HighchartsData/master/gas_throughcap.json'
-var githubPoints = JSON.parse(JSON.stringify(JSON.parse(getData(urlPoints))));
-var githubSeries = JSON.parse(JSON.stringify(JSON.parse(getData(urlSeries))));
 var commodity = 'gas'
 
-var [pointMap,pointData] = commodityGraph(commodity,githubPoints,githubSeries) //this is called destructive assignment
+var [pointMap,pointData] = commodityGraph(commodity) //this is called destructive assignment
 
 var select_pipelines = document.getElementById('select_pipelines');
 select_pipelines.addEventListener('change', (select_pipelines) => {
