a03c0eb2ec62a0909f9e092556c8698439c78e68
Andrew Moss <andrew@m0ss.dev>
2020-10-06 17:01:39 -0600

    feat: Wds and nginx working

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,35 @@
+const path = require('path')
+const CopyPlugin = require("copy-webpack-plugin");
+
 module.exports = {
-    mode: 'production',
-    entry: './src/index.js',
-    module: {
-        rules: [
-            {
-                test: /\.(js)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['*', '.js']
-    },
-    output: {
-        path: __dirname + '/dist',
-        publicPath: '/',
-        filename: 'bundle.js'
-    },
-    devServer: {
-        compress: true,
-        port: 8080
-    },
+  mode: "production",
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js"],
+  },
+  output: {
+    path: __dirname + "/dist",
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  devServer: {
+    compress: true,
+    port: 8080,
+    historyApiFallback: true,
+    contentBase: './'
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "public", to: "./" }],
+    }),
+  ],
 };
