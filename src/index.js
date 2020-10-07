1e4e76b75d82cc4c27f93af2e5deff92d259a09c
Grant Moss <gmoss@ualberta.ca>
2020-10-07 10:04:23 -0600

    added webpack and babel

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import { kevinCrudeProduction } from "./Kevin/crude_production/crude_production.js";
-import { kevinCrudeExports } from "./Kevin/crude_exports/crude_exports.js";
-import { kevinUsImports } from "./Kevin/us_imports/us_imports.js";
-import { kevinCrudePrices } from "./Kevin/crude_prices/crude_prices.js";
-import { coletteCrudeByRail } from "./Colette/crude_by_rail/crude_by_rail.js";
-import { coletteCrudeTakeaway } from "./Colette/crude_takeaway/crude_takeaway.js";
-import { saraGasTraffic } from "./Sara/gas_traffic/gas_traffic.js";
-import { cassandraAllPipes } from "./Cassandra/all_pipes/pipeline_metrics.js";
-import { ryanNglExports } from "./Ryan/ngl_exports/ngl_exports.js";
-import { jenniferFinResources } from "./Jennifer/financial_instruments/fin_resource.js";
+import { kevinCrudeProduction } from "./Kevin/crude_production/crude_production";
+import { kevinCrudeExports } from "./Kevin/crude_exports/crude_exports";
+import { kevinUsImports } from "./Kevin/us_imports/us_imports";
+import { kevinCrudePrices } from "./Kevin/crude_prices/crude_prices";
+import { coletteCrudeByRail } from "./Colette/crude_by_rail/crude_by_rail";
+import { coletteCrudeTakeaway } from "./Colette/crude_takeaway/crude_takeaway";
+import { saraGasTraffic } from "./Sara/gas_traffic/gas_traffic";
+import { cassandraAllPipes } from "./Cassandra/all_pipes/pipeline_metrics";
+import { ryanNglExports } from "./Ryan/ngl_exports/ngl_exports";
+import { jenniferFinResources } from "./Jennifer/financial_instruments/fin_resource";
 
 kevinCrudeProduction();
 kevinCrudeExports();
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+    mode: "production",
+  entry: {
+    app: "./src/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js"],
+  },
+};
