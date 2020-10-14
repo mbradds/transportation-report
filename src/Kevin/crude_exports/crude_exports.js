87b5bd1506f889b48233a3188cd5ced6efdf163f
Grant Moss <gmoss@ualberta.ca>
2020-10-14 17:25:07 -0600

    added padd map to crude exports

--- src/Kevin/crude_exports/crude_exports.js
+++ src/Kevin/crude_exports/crude_exports.js
@@ -1,4 +1,9 @@
-import { cerPalette, fillDropUpdate,getUnique, prepareSeriesTidy } from "../../modules/util.js";
+import {
+  cerPalette,
+  fillDropUpdate,
+  getUnique,
+  prepareSeriesTidy,
+} from "../../modules/util.js";
 
 import crudeExportsData from "./crude-oil-exports-by-destination-annual.json";
 
@@ -14,7 +19,12 @@ export const kevinCrudeExports = () => {
 
   var crudeExportFilters = { Unit: "bbl/d" };
 
-  fillDropUpdate("select_units_crude_exports",getUnique(crudeExportsData,"Unit"),false,"bbl/d")
+  fillDropUpdate(
+    "select_units_crude_exports",
+    getUnique(crudeExportsData, "Unit"),
+    false,
+    "bbl/d"
+  );
 
   var seriesData = prepareSeriesTidy(
     crudeExportsData,
@@ -25,13 +35,158 @@ export const kevinCrudeExports = () => {
     crudeExportColors
   );
 
-  const createCrudeExportsChart = (seriesData) => {
+  var selectedPadd = { padd: null };
+
+  const createPaddMap = () => {
+    var paddMap = new Highcharts.mapChart("container_padd_map", {
+      chart: {
+        type: "map",
+        map: "countries/us/custom/us-all-mainland",
+      },
+
+      title: {
+        text: null,
+      },
+
+      credits: {
+        enabled: false,
+      },
+
+      legend: {
+        enabled: false,
+      },
+
+      tooltip: {
+        formatter: function () {
+          return this.series.name;
+        },
+      },
+
+      plotOptions: {
+        map: {
+          allAreas: false,
+        },
+        series: {
+          states: {
+            inactive: {
+              opacity: 0.5,
+            },
+            hover: {
+              brightness: 0,
+            },
+          },
+          events: {
+            mouseOver: function () {
+              var currentSelection = this.name;
+              selectedPadd.padd = currentSelection;
+              this.chart.series.forEach(function (s) {
+                if (s.name != currentSelection) {
+                  s.setState("inactive");
+                } else {
+                  s.setState("hover");
+                }
+              });
+            },
+            mouseOut: function () {
+              this.chart.series.forEach(function (s) {
+                s.setState("");
+              });
+            },
+          },
+        },
+      },
+
+      series: [
+        {
+          name: "PADD I",
+          data: [
+            ["us-me", 1],
+            ["us-vt", 1],
+            ["us-nh", 1],
+            ["us-ma", 1],
+            ["us-ct", 1],
+            ["us-ri", 1],
+            ["us-ny", 1],
+            ["us-pa", 1],
+            ["us-nj", 1],
+            ["us-de", 1],
+            ["us-md", 1],
+            ["us-wv", 1],
+            ["us-va", 1],
+            ["us-nc", 1],
+            ["us-sc", 1],
+            ["us-ga", 1],
+            ["us-fl", 1],
+          ],
+          color: crudeExportColors["PADD I"],
+        },
+        {
+          name: "PADD II",
+          data: [
+            ["us-nd", 1],
+            ["us-sd", 1],
+            ["us-ne", 1],
+            ["us-ks", 1],
+            ["us-ok", 1],
+            ["us-mn", 1],
+            ["us-ia", 1],
+            ["us-mo", 1],
+            ["us-wi", 1],
+            ["us-il", 1],
+            ["us-mi", 1],
+            ["us-in", 1],
+            ["us-ky", 1],
+            ["us-tn", 1],
+            ["us-oh", 1],
+          ],
+          color: crudeExportColors["PADD II"],
+        },
+        {
+          name: "PADD III",
+          data: [
+            ["us-tx", 1],
+            ["us-la", 1],
+            ["us-nm", 1],
+            ["us-ms", 1],
+            ["us-al", 1],
+            ["us-ar", 1],
+          ],
+          color: crudeExportColors["PADD III"],
+        },
+        {
+          name: "PADD IV",
+          data: [
+            ["us-mt", 1],
+            ["us-id", 1],
+            ["us-ut", 1],
+            ["us-co", 1],
+            ["us-wy", 1],
+          ],
+          color: crudeExportColors["PADD IV"],
+        },
+        {
+          name: "PADD V",
+          data: [
+            ["us-wa", 1],
+            ["us-or", 1],
+            ["us-ca", 1],
+            ["us-nv", 1],
+            ["us-az", 1],
+          ],
+          color: crudeExportColors["PADD V"],
+        },
+      ],
+    });
+    return paddMap;
+  };
+
+  const createCrudeExportsChart = (seriesData, paddMap) => {
     var chartCrudeExports = new Highcharts.chart("container_crude_exports", {
       chart: {
         type: "column", //line,bar,scatter,area,areaspline
-        zoomType: "x", //allows the user to focus in on the x or y (x,y,xy)
-        borderColor: "black",
-        borderWidth: 1,
+        //zoomType: "x", //allows the user to focus in on the x or y (x,y,xy)
+        //borderColor: "black",
+        //borderWidth: 1,
         animation: true,
         events: {
           load: function () {
@@ -58,6 +213,26 @@ export const kevinCrudeExports = () => {
           stacking: "normal",
           dataLabels: {
             enabled: false,
+          },
+        },
+        series: {
+          events: {
+            mouseOver: function () {
+              var currentSelection = this.name;
+              selectedPadd.padd = currentSelection;
+              paddMap.series.forEach(function (s) {
+                if (s.name != currentSelection) {
+                  s.setState("inactive");
+                } else {
+                  s.setState("hover");
+                }
+              });
+            },
+            mouseOut: function () {
+              paddMap.series.forEach(function (s) {
+                s.setState("");
+              });
+            },
           },
         },
       },
@@ -95,11 +270,9 @@ export const kevinCrudeExports = () => {
     return chartCrudeExports;
   };
 
-  var chartCrudeExports = createCrudeExportsChart(seriesData);
+  var paddMap = createPaddMap();
+  var chartCrudeExports = createCrudeExportsChart(seriesData, paddMap);
 
-  /**
-   * @description Method description
-   */
   var selectUnitsCrudeExports = document.getElementById(
     "select_units_crude_exports"
   );
@@ -125,4 +298,8 @@ export const kevinCrudeExports = () => {
       });
     }
   );
+  // var x = true;
+  // while (x == true) {
+  //   console.log(selectedPadd);
+  // }
 };
