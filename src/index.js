2780822b8e9d8080effd18bd726c72d6fdff4134
Grant Moss <gmoss@ualberta.ca>
2020-10-20 17:45:22 -0600

    started negotiated settlements

--- src/index.js
+++ src/index.js
@@ -66,6 +66,7 @@ import { rebeccaGasProd } from "./Rebecca/gas_production/gas_production";
 import { rebeccaGasTrade } from "./Rebecca/gas_trade/gas_trade"
 import { cassandraAllPipes } from "./Cassandra/all_pipes/pipeline_metrics";
 import { cassandraTolls } from "./Cassandra/tolls/tolls";
+import { cassandraSettlements } from "./Cassandra/negotiated_settlements/settlements";
 import { ryanNglExports } from "./Ryan/ngl_exports/ngl_exports";
 import { jenniferFinResources } from "./Jennifer/financial_instruments/fin_resource";
 
@@ -83,5 +84,6 @@ rebeccaGasProd();
 rebeccaGasTrade();
 cassandraAllPipes();
 cassandraTolls();
+cassandraSettlements();
 ryanNglExports();
 jenniferFinResources();
