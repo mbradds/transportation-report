908ee1b8511c5c3363c14046f1bbfada1d16a168
Grant Moss <gmoss@ualberta.ca>
2020-10-25 11:10:33 -0600

    fixed bug on gantt rolldown

--- src/Cassandra/negotiated_settlements/settleSeries.js
+++ src/Cassandra/negotiated_settlements/settleSeries.js
@@ -56,6 +56,24 @@ const getEndDate = (date) => {
   }
 };
 
+function sortByProperty(property) {
+  return function (a, b) {
+    if (a[property] > b[property]) return 1;
+    else if (a[property] < b[property]) return -1;
+
+    return 0;
+  };
+}
+
+const applyEndDateColors = (data) => {
+  return data.map((row, rowNum) => {
+    var [endDate, seriesColor] = getEndDate(row["End Date"]);
+    row.color = seriesColor;
+    row.end = endDate;
+    return row;
+  });
+};
+
 const settlementSeries = (data, filters) => {
   var seriesTracker = {};
   var seriesSettle = [];
@@ -65,95 +83,129 @@ const settlementSeries = (data, filters) => {
     data = data.filter((row) => row.Commodity == filters.Commodity);
   }
 
+  data = applyEndDateColors(data);
+  data = data.sort(sortByProperty("end"));
+  console.log(data);
+
   data.map((row, rowNum) => {
     dates.push(row["Start Date"]);
     dates.push(row["End Date"]);
-    var [endDate, seriesColor] = getEndDate(row["End Date"]);
 
     if (seriesTracker.hasOwnProperty(row.Company)) {
       //the parent company is already in the series, add the sub settlement
       seriesTracker[row.Company].startDate.push(row["Start Date"]);
-      seriesTracker[row.Company].endDate.push(endDate);
+      seriesTracker[row.Company].endDate.push(row.end);
       seriesSettle.push({
         name: row["Settlement Name"],
         id: row["Settlement Name"],
         parent: row.Company,
-        color: seriesColor,
+        color: row.color,
         start: row["Start Date"],
-        end: endDate,
+        end: row.end,
       });
     } else {
       //A new company is added to the series as the parent, and the current settlement is also added
       seriesTracker[row.Company] = {
         startDate: [row["Start Date"]],
-        endDate: [endDate],
+        endDate: [row.end],
       };
       seriesSettle.push({
         name: row["Settlement Name"],
         id: row["Settlement Name"],
         parent: row.Company,
-        color: seriesColor,
+        color: row.color,
         start: row["Start Date"],
-        end: endDate,
+        end: row.end,
       });
     }
   });
 
   const companySettles = [];
 
+  const companyCounter = (companyTracker, company) => {
+    if (companyTracker.hasOwnProperty(company)) {
+      companyTracker[company]++;
+    } else {
+      companyTracker[company] = 1;
+    }
+    return companyTracker;
+  };
+
+  const companyId = (companyTracker, company) => {
+    if (companyTracker.hasOwnProperty(company)) {
+      return company + "_" + companyTracker[company];
+    } else {
+      return company;
+    }
+  };
+
+  var companyTracker = {}; //checks if a company has already been added so that the ID can be changed for other bars
   for (const company in seriesTracker) {
     var companyStartDates = seriesTracker[company].startDate;
     var companyEndDates = seriesTracker[company].endDate;
     var currentStart = companyStartDates[0];
-    var currentValid = 0
     companyEndDates.map((endDate, endNum) => {
       if (companyStartDates[endNum + 1] - endDate > 86400000) {
-        //console.log(companyStartDates[endNum + 1] - endDate)
         companySettles.push({
           name: company,
           collapsed: true,
           color: cerPalette["Night Sky"],
-          id: company,
+          id: companyId(companyTracker, company),
           start: currentStart,
           end: companyEndDates[endNum],
         });
+        companyTracker = companyCounter(companyTracker, company);
         currentStart = companyStartDates[endNum + 1];
       } else {
-          if (endNum == companyEndDates.length-1){
-            companySettles.push({
-                name: company,
-                collapsed: true,
-                color: cerPalette["Night Sky"],
-                id: company,
-                start: currentStart,
-                end: companyEndDates[endNum],
-              });
-          }
+        if (endNum == companyEndDates.length - 1) {
+          companySettles.push({
+            name: company,
+            collapsed: true,
+            color: cerPalette["Night Sky"],
+            id: companyId(companyTracker, company),
+            start: currentStart,
+            end: companyEndDates[endNum],
+          });
+          companyTracker = companyCounter(companyTracker, company);
+        }
       }
     });
   }
-  
+
   dates = dates.filter((row) => row !== null);
 
-  return [[...seriesSettle, ...companySettles],dates];
+  return [[...seriesSettle, ...companySettles], dates];
 };
 
-const [seriesData,dates] = settlementSeries(
-    settlementsData,
-    filters
-  );
+// const oneCompany = (seriesData,company='Enbridge Pipelines Inc.') => {
+//   const companySeries = seriesData.filter((row,rowNum)=> {
+//     if (row.name == company || row.parent == company) {
+//       return row
+//     }
+//   })
+//   return companySeries
+// }
 
-//console.log(seriesData)
+// const sortSeries = (seriesData) => {
+//   seriesData.sort(function(a,b){
+//     return a.end - b.end
+//   })
+// }
+
+var [seriesData, dates] = settlementSeries(settlementsData, filters);
+console.log(seriesData);
+
+// const nova = oneCompany(seriesData,'NOVA Gas Transmission Ltd.')
+// console.log(nova)
 
 const createSettlements = (seriesData) => {
-
   return Highcharts.ganttChart("container_settlements", {
     chart: {
       type: "gantt",
       borderWidth: 1,
     },
     yAxis: {
-    	uniqueNames:true
+      uniqueNames: true,
     },
     xAxis: [
       {
@@ -169,7 +221,7 @@ const createSettlements = (seriesData) => {
       },
     ],
     tooltip: {
-        xDateFormat: "%Y-%m-%d",
+      xDateFormat: "%Y-%m-%d",
     },
 
     series: [
@@ -183,5 +235,3 @@ const createSettlements = (seriesData) => {
 };
 
 const settlementChart = createSettlements(seriesData);
-
-
