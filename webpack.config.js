df232a0dae1cc0f1b626dd8d728ac24d6a9cbd3f
mbradds <gmoss@ualberta.ca>
2020-11-29 17:26:29 -0700

    added french bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,20 @@
 const path = require("path");
 const JsonMinimizerPlugin = require("json-minimizer-webpack-plugin");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
-  .BundleAnalyzerPlugin;
+// const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+//   .BundleAnalyzerPlugin;
 
 module.exports = {
   mode: "production",
   target: "es5",
-  entry: "./src/index_eng.js",
+  //entry: "./src/index_eng.js",
+  entry: {
+    eng: "./src/index_eng.js",
+    fra: "./src/index_fra.js",
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    filename: "bundle_[name].js",
+    //filename: "bundle.js",
   },
 
   //plugins: [new BundleAnalyzerPlugin(),new JsonMinimizerPlugin()],
