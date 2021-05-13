b6a26c0bbb0d3dc681a6668cad376d4900915368
grant <grantmossdev@gmail.com>
2021-05-13 12:00:24 -0600

    added copy/clean webpack pattern and added a few missing translations

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 // const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
 //   .BundleAnalyzerPlugin;
 
@@ -13,13 +15,38 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle_[name].js",
   },
-  // plugins: [
-  //   new BundleAnalyzerPlugin({
-  //     analyzerMode: "server",
-  //     openAnalyzer: true,
-  //     analyzerPort: 8888,
-  //   }),
-  // ],
+  plugins: [
+    //   new BundleAnalyzerPlugin({
+    //     analyzerMode: "server",
+    //     openAnalyzer: true,
+    //     analyzerPort: 8888,
+    //   }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "main.css"),
+          to: path.resolve(__dirname, "dist", "main.css"),
+        },
+        {
+          from: path.resolve(__dirname, "src", "eng.html"),
+          to: path.resolve(__dirname, "dist", "eng.html"),
+        },
+        {
+          from: path.resolve(__dirname, "src", "fra.html"),
+          to: path.resolve(__dirname, "dist", "fra.html"),
+        },
+        {
+          from: path.resolve(__dirname, "src", "GCWeb"),
+          to: path.resolve(__dirname, "dist", "GCWeb"),
+        },
+        {
+          from: path.resolve(__dirname, "src", "wet-boew"),
+          to: path.resolve(__dirname, "dist", "wet-boew"),
+        },
+      ],
+    }),
+    new CleanWebpackPlugin(),
+  ],
   module: {
     rules: [
       {
